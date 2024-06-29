62c751c50f8cb7fa877906d49baf515b9a068259
Vivek4104 <vivekprajapati4104@gmail.com>
2024-06-29 14:06:05 +0530

    blogwebsite initial

---
+++ models/blog.js
@@ -0,0 +1,14 @@
+const {Schema,model} = require("mongoose");
+
+
+
+const blogSchema = new Schema({
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    createdBy: { type: Schema.Types.ObjectId, ref: "user", },
+    coverImageURL: { type: String, required: false },
+
+
+},{timestamps:true});
+const Blog = model('blog',blogSchema);
+module.exports =Blog;
